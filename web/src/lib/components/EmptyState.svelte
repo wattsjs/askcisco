c53c7c5191a0bf5b43d92a359024116d087b2af6
Jamie Watts <jamie@wattsjs.com>
2023-07-16 20:32:44 +1000

    typo

--- web/src/lib/components/EmptyState.svelte
+++ web/src/lib/components/EmptyState.svelte
@@ -4,10 +4,9 @@
     This is an experimental open source AI chatbot that leverages Cisco's public documentation as context to OpenAI's GPT-3.5 API.
   </p>
   <p class="mb-2 leading-normal text-muted-foreground">
-    Use me to ask technical questions about Cisco products and technologies.
+    Use me to answer technical questions about Cisco products and technologies.
     I am not a search engine - I only know answers to questions that are in the documentation I have been trained on.
   </p>
-
   <p class="mb-2 leading-normal text-muted-foreground">
     Every response will reference some source documents that will be listed below the answer. 
     You can click on any of the links to validate my responses.
